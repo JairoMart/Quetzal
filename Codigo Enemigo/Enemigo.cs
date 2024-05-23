b758f6facc3c42ad60630bcea2c3fc9e1495f459
JairoMart <160837626+JairoMart@users.noreply.github.com>
2024-05-23 10:09:19 +0200

    cambios

---
+++ Codigo Enemigo/Enemigo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] private float vida;
+    //private Animator animator;
+
+    //private void Start()
+    //{
+    //    animator = GetComponent<animator>();
+    //}
+
+    public void TomarDaño(float daño)
+    {
+        vida -= daño;
+
+        if(vida <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
